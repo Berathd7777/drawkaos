843b55ec193778f3af1346b9ff327e9e44fd3a53
Cristhian Duran <durancristhian@gmail.com>
2021-05-04 12:44:49 -0300

    start with turns

--- flows/room/RoomActions.tsx
+++ flows/room/RoomActions.tsx
@@ -1,4 +1,5 @@
 import { Alert, AlertIcon, Box, Button, Spinner } from '@chakra-ui/react'
+import { usePlayers } from 'contexts/Players'
 import { useRoom } from 'contexts/Room'
 import React from 'react'
 import { REMOTE_DATA } from 'types/RemoteData'
@@ -7,12 +8,20 @@ import { updateRoom } from 'utils/updateRoom'
 
 export function RoomActions() {
   const { status, error, data } = useRoom()
+  const {
+    status: statusPlayers,
+    error: errorPlayers,
+    data: players,
+  } = usePlayers()
 
-  if (status === REMOTE_DATA.IDLE || status === REMOTE_DATA.LOADING) {
+  if (
+    (status || statusPlayers) === REMOTE_DATA.IDLE ||
+    (status || statusPlayers) === REMOTE_DATA.LOADING
+  ) {
     return <Spinner />
   }
 
-  if (status === REMOTE_DATA.ERROR) {
+  if ((status || errorPlayers) === REMOTE_DATA.ERROR) {
     return (
       <Alert status="error">
         <AlertIcon />
@@ -25,6 +34,25 @@ export function RoomActions() {
     if (!data) {
       return
     }
+
+    const ids = players.map(({ id }) => id)
+
+    /*
+      Personas que van a jugar
+      ['cris', 'cami', 'pato', 'juli']
+
+                Línea 1   Línea 2   Línea 3   Línea 4
+      Turno 1   cris      cami      pato      juli
+      Turno 2   cami      pato      juli      cris
+      Turno 3   pato      juli      cris      cami
+      Turno 4   juli      cris      cami      pato
+    */
+
+    const result = ids.reduce((acc, currId, currIdx, arr) => {
+      return acc
+    }, [])
+
+    console.log(result)
 
     updateRoom({
       id: data.id,
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,33 @@ import { CreateRoomForm } from 'flows/home/CreateRoomForm'
 import React from 'react'
 
 function Home() {
+  /* const ids = [
+    { id: 'Cris' },
+    { id: 'Cami' },
+    { id: 'Pato' },
+    { id: 'Juli' },
+  ].map(({ id }) => id) */
+
+  /*
+    Personas que van a jugar
+    ['cris', 'cami', 'pato', 'juli']
+
+              Línea 1   Línea 2   Línea 3   Línea 4
+    Turno 1   cris      cami      pato      juli
+    Turno 2   cami      pato      juli      cris
+    Turno 3   pato      juli      cris      cami
+    Turno 4   juli      cris      cami      pato
+  */
+
+  /* const lines = ids.reduce((acc, currId, currIdx, arr) => {
+    const others = [...arr.slice(currIdx + 1), ...arr.slice(0, currIdx)]
+    const line = [currId, ...others]
+
+    return [...acc, line]
+  }, [])
+
+  console.log(lines) */
+
   return (
     <Stack spacing="8">
       <Heading as="h1">gartic-phone</Heading>
