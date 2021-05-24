00304a50feb5a3093d5326513bfd85c8837d42bd
Cristhian Duran <durancristhian@gmail.com>
2021-05-24 19:28:18 -0300

    Remove error pages

--- pages/404.tsx
+++
@@ -1,8 +0,0 @@
-import { AlertMessage } from 'components/AlertMessage'
-import React from 'react'
-
-function Error() {
-  return <AlertMessage status="error" title="Page not found" />
-}
-
-export default Error
