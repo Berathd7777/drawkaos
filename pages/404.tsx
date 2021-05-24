44cc87da3ef997d8ca4fce1eb2689c9c4c4ce801
Cristhian Duran <durancristhian@gmail.com>
2021-05-24 19:25:16 -0300

    Add 404

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+import { AlertMessage } from 'components/AlertMessage'
+import React from 'react'
+
+function Error() {
+  return <AlertMessage status="error" title="Page not found" />
+}
+
+export default Error
