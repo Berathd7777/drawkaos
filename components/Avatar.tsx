0b6f6349c125f95e260bf0ad659d17ff147132fb
Cristhian Duran <durancristhian@gmail.com>
2021-05-28 10:47:44 -0300

    Add boring avatars

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,32 +1,23 @@
-import { Image, Spinner, Stack } from '@chakra-ui/react'
+import BoringAvatar from 'boring-avatars'
 import React, { useMemo } from 'react'
 import StringSanitizer from 'string-sanitizer'
 
 type Props = {
   seed: string
-  size?: string
+  size?: number
 }
 
-export function Avatar({ seed, size = '64px' }: Props) {
+export function Avatar({ seed, size = 64 }: Props) {
   const text = useMemo(() => {
     return StringSanitizer.sanitize(seed.toLocaleLowerCase())
   }, [seed])
 
   return (
-    <Image
-      src={`https://avatars.dicebear.com/api/bottts/${text}.svg`}
-      height={size}
-      width={size}
-      fallback={
-        <Stack
-          height={size}
-          width={size}
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Spinner />
-        </Stack>
-      }
+    <BoringAvatar
+      size={size}
+      name={text}
+      variant="beam"
+      colors={['#9D9F89', '#84AF97', '#8BC59B', '#B2DE93', '#CCEE8D']}
     />
   )
 }
